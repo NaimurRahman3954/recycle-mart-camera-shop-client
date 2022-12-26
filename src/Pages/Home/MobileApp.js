e0f1c04874b565268051a3f3656acc03aa322410
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-26 23:36:12 +0600

    mobile app section added

--- src/Pages/Home/MobileApp.js
+++ src/Pages/Home/MobileApp.js
@@ -3,17 +3,27 @@ import './MobileApp.css'
 
 const MobileApp = () => {
   return (
-    <div className="container mx-auto my-16">
-      <div className="flex flex-wrap">
-        <div className="basis-1/2 grid place-items-center">
-          <h1 className="text-5xl font-bold mb-6">Download our Mobile App</h1>
-        </div>
+    <div
+      className="container mx-auto my-16"
+      style={{
+        width: '900px',
+        height: '800px',
+        backgroundImage:
+          'url(https://sites.google.com/site/idontgetitbutton/_/rsrc/1444103808278/home/blog/untitledpost/appstorebadges.png))',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+      }}
+    >
+      <h1 className="text-5xl font-bold mb-16">Download our Mobile App</h1>
+      <div className="flex flex-wrap justify-center lg:mx-16">
         <div className="basis-1/2">
           <div className="mobile-phone">
             <div className="brove">
               <span class="speaker"></span>
             </div>
             <div className="screen grid place-items-center gap-0">
+              {/* <p>Download our Mobile App</p> */}
               <img
                 src="https://sites.google.com/site/idontgetitbutton/_/rsrc/1444103808278/home/blog/untitledpost/appstorebadges.png"
                 alt=""
