10071569c05d0a9861e237f18ef21b2ea64ceef1
NaimurRahman3954 <mnrahaman3954@gmail.com>
2023-01-18 01:43:03 +0600

    UI updated

--- src/Pages/Home/MobileApp.js
+++ src/Pages/Home/MobileApp.js
@@ -4,18 +4,16 @@ import './MobileApp.css'
 const MobileApp = () => {
   return (
     <div
-      className="pb-6 pt-1 my-72 h-72"
+      className="pb-6 pt-1"
       style={{
-        backgroundImage: `url(https://i.ibb.co/vY6f8pf/background.jpg)`,
+        backgroundImage: `url(https://wallpaperaccess.com/full/1594672.jpg)`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center center',
       }}
     >
       <div className="container mx-auto my-48">
-        <h1 className="text-5xl font-bold mb-16 -mt-[420px]">
-          Download our Mobile App 📲
-        </h1>
+        <h1 className="text-5xl font-bold mb-16">Download our Mobile App 📲</h1>
         <div className="mobile-phone bg-white">
           <div className="brove">
             <span class="speaker"></span>
