3e0ccc5964faa6eee969bad1f1f10bbeb8eb92f6
NaimurRahman3954 <mnrahaman3954@gmail.com>
2023-01-28 23:04:30 +0600

    UI updated

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className="flex justify-center items-center">
-      <button className="btn loading bg-primary my-16">loading</button>
+      <button className="btn loading bg-warning my-16">loading</button>
     </div>
   )
 }
