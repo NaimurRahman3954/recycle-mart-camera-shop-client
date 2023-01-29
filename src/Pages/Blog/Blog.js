126238b7832e531cd8905df658f94820c10dabb9
NaimurRahman3954 <mnrahaman3954@gmail.com>
2023-01-29 22:54:44 +0600

    UI updated

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,80 +1,14 @@
 import React from 'react'
 import BlogCard1 from './BlogCard1'
+import BlogCard2 from './BlogCard2'
 
 const Blog = () => {
   return (
     <div className="mt-5">
       {/* 📌📌 Blog 1 */}
       <BlogCard1></BlogCard1>
-      <div className="text-gray-900 m-5">
-        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm bg-base-200">
-          <div className="flex items-center justify-between">
-            <span className="text-sm dark:text-gray-600">Nov 24, 2022</span>
-            <button className="font-bold rounded btn-error px-3 py-1 my-2">
-              React
-            </button>
-          </div>
-          <div className="mt-3">
-            <h3 className="text-2xl font-bold hover:underline">
-              What are the different ways to manage state in a React
-              application?
-            </h3>
-            <p className="mt-2 text-justify">
-              In React apps, there are at least seven ways to handle the state.
-              Let us briefly explore a few of them in this part.
-            </p>
-            <ol className="list-decimal pl-9 pt-2">
-              <li>URL</li>
-              <li>Web Storage</li>
-              <li>Local State</li>
-              <li>Lifted State</li>
-              <li>Derived State</li>
-            </ol>
-          </div>
-          <div className="flex items-center justify-between mt-4">
-            <a
-              href="https://blog.saeloun.com/2021/11/11/8-ways-to-handle-react-state-part1.html"
-              className="hover:underline text-blue-700"
-            >
-              Read more →
-            </a>
-          </div>
-        </div>
-      </div>
       {/* 📌📌 Blog 2 */}
-      <div className="text-gray-900 m-5">
-        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm bg-base-200">
-          <div className="flex items-center justify-between">
-            <span className="text-sm dark:text-gray-600">Nov 24, 2022</span>
-            <button className="font-bold rounded btn-error px-3 py-1 my-2">
-              Javascript
-            </button>
-          </div>
-          <div className="mt-3">
-            <h3 className="text-2xl font-bold hover:underline text-justify">
-              How does prototypical inheritance work?
-            </h3>
-            <p className="mt-2 text-justify">
-              Every object with its methods and properties contains an internal
-              and hidden property known as [[Prototype]]. The Prototypal
-              Inheritance is a feature in javascript used to add methods and
-              properties in objects. It is a method by which an object can
-              inherit the properties and methods of another object.
-              Traditionally, in order to get and set the [[Prototype]] of an
-              object, we use Object.getPrototypeOf and Object.setPrototypeOf.
-              Nowadays, in modern language, it is being set using __proto__.
-            </p>
-          </div>
-          <div className="flex items-center justify-between mt-4">
-            <a
-              href="https://www.geeksforgeeks.org/prototypal-inheritance-using-__proto__-in-javascript/"
-              className="hover:underline text-blue-700"
-            >
-              Read more →
-            </a>
-          </div>
-        </div>
-      </div>
+      <BlogCard2></BlogCard2>
       {/* 📌📌 Blog 3 */}
       <div className="text-gray-900 m-5">
         <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm bg-base-200">
