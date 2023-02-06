6b1557282267fd20a12a822a9fb74027e94baf61
NaimurRahman3954 <mnrahaman3954@gmail.com>
2023-02-06 14:35:37 +0600

    updating UI

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -73,7 +73,7 @@ const Header = () => {
         >
           <ul
             className={`menu menu-horizontal p-0 text-${
-              isHome ? 'white' : 'primary'
+              isHome ? 'white' : 'black'
             }`}
           >
             {menuItems}
